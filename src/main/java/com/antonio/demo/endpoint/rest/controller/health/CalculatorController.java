75960d807213aa275cca523f1118d3c6cc429b81
MamitianaAntonio <ratolojanaharyantonio1@gmail.com>
2026-05-27 20:25:22 +0300

    feat : format code using script and modify calling methods on test

--- src/main/java/com/antonio/demo/endpoint/rest/controller/health/CalculatorController.java
+++ src/main/java/com/antonio/demo/endpoint/rest/controller/health/CalculatorController.java
@@ -6,27 +6,26 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CalculatorController {
-    @GetMapping("/add")
-    public double add(@RequestParam double a, @RequestParam double b) {
-        return a + b;
-    }
+  @GetMapping("/add")
+  public double add(@RequestParam double a, @RequestParam double b) {
+    return a + b;
+  }
 
-    @GetMapping("/subtract")
-    public double subtract(@RequestParam double a, @RequestParam double b) {
-        return a - b;
-    }
+  @GetMapping("/subtract")
+  public double subtract(@RequestParam double a, @RequestParam double b) {
+    return a - b;
+  }
 
-    @GetMapping("/multiply")
-    public double multiply(@RequestParam double a, @RequestParam double b) {
-        return a * b;
-    }
+  @GetMapping("/multiply")
+  public double multiply(@RequestParam double a, @RequestParam double b) {
+    return a * b;
+  }
 
-    @GetMapping("/divide")
-    public double divide(@RequestParam double a, @RequestParam double b) {
-        if (b == 0) {
-            throw new ArithmeticException("Division with zero is impossible");
-        }
-        return a / b;
+  @GetMapping("/divide")
+  public double divide(@RequestParam double a, @RequestParam double b) {
+    if (b == 0) {
+      throw new ArithmeticException("Division with zero is impossible");
     }
-
+    return a / b;
+  }
 }
--- src/test/java/com/antonio/demo/endpoint/rest/controller/health/CalculatorTest.java
+++ src/test/java/com/antonio/demo/endpoint/rest/controller/health/CalculatorTest.java
@@ -7,36 +7,35 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class CalculatorTest {
-    private CalculatorController calculator;
+  private CalculatorController calculator;
 
-    @BeforeEach
-    public void setUp() {
-        calculator = new CalculatorController();
-    }
+  @BeforeEach
+  public void setUp() {
+    calculator = new CalculatorController();
+  }
 
-    @Test
-    public void testAdd() {
-        assertEquals(8.0, calculator.add(5, 3));
-    }
-    
-    @Test
-    public void testSubstract () {
-        assertEquals(2.0, calculator.subtract(5, 3));
-    }
-    
-    @Test
-    public void testMultiply () {
-        assertEquals(15, calculator.multiply(5, 3));
-    }
-    
-    @Test
-    public void testDivide () {
-        assertEquals(2.5, calculator.divide(5, 2));
-    }
-    
-    @Test
-    public void testDivideByZero() {
-        assertThrows(ArithmeticException.class, () -> calculator.divide(5, 0));
-    }
+  @Test
+  public void testAdd() {
+    assertEquals(8.0, calculator.add(5, 3));
+  }
 
+  @Test
+  public void testSubstract() {
+    assertEquals(2.0, calculator.subtract(5, 3));
+  }
+
+  @Test
+  public void testMultiply() {
+    assertEquals(15, calculator.multiply(5, 3));
+  }
+
+  @Test
+  public void testDivide() {
+    assertEquals(2.5, calculator.divide(5, 2));
+  }
+
+  @Test
+  public void testDivideByZero() {
+    assertThrows(ArithmeticException.class, () -> calculator.divide(5, 0));
+  }
 }
