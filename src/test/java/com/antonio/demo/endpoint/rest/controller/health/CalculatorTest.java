88e638b9617a074ef259f4dad571e01af176f84d
MamitianaAntonio <ratolojanaharyantonio1@gmail.com>
2026-05-27 19:32:37 +0300

    feat : write simple test for arithmetic poja

---
+++ src/test/java/com/antonio/demo/endpoint/rest/controller/health/CalculatorTest.java
@@ -0,0 +1,40 @@
+package com.antonio.demo.endpoint.rest.controller.health;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class CalculatorTest {
+    @BeforeEach
+    public void setUp() {
+        calculator = new CalculatorController();
+    }
+
+    @Test
+    public void testAdd() {
+        assertEquals(8.0, calculator.add(5, 3));
+    }
+    
+    @Test
+    public void testSubstract () {
+        assertEquals(2.0, calculator.substract(5, 3));
+    }
+    
+    @Test
+    public void testMultiply () {
+        assertEquals(15, calculator.multiply());
+    }
+    
+    @Test
+    public void testDivide () {
+        assertEquals(2.5, calculator.divide(5, 2));
+    }
+    
+    @Test
+    public void testDivideByZero() {
+        assertThrows(ArithmeticException.class, () -> calculator.divide(5, 0));
+    }
+
+}
