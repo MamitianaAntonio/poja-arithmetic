2d0a821816b8126c70fe04a15f7c0d4a23548b0a
MamitianaAntonio <ratolojanaharyantonio1@gmail.com>
2026-06-01 12:17:11 +0300

    fix : fix format for arith service

--- src/main/java/com/antonio/demo/endpoint/rest/service/CalculatorService.java
+++ src/main/java/com/antonio/demo/endpoint/rest/service/CalculatorService.java
@@ -1,9 +1,7 @@
 package com.antonio.demo.endpoint.rest.service;
 
-import com.antonio.demo.PojaGenerated;
 import org.springframework.stereotype.Service;
 
-@PojaGenerated
 @Service
 public class CalculatorService {
   public long arithAdd(long a, long b) {
