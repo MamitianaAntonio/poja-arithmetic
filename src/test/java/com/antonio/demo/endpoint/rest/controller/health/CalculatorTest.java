0dc25044ccd3d47a5b0bab8582c6d479382c6041
MamitianaAntonio <ratolojanaharyantonio1@gmail.com>
2026-06-01 12:23:49 +0300

    feat : implentation of new test usign injection by controller

--- src/test/java/com/antonio/demo/endpoint/rest/controller/health/CalculatorTest.java
+++ src/test/java/com/antonio/demo/endpoint/rest/controller/health/CalculatorTest.java
@@ -3,42 +3,60 @@ package com.antonio.demo.endpoint.rest.controller.health;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import com.antonio.demo.endpoint.rest.service.CalculatorService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class CalculatorTest {
   private CalculatorController calculator;
+  private CalculatorService arithService;
 
   @BeforeEach
   public void setUp() {
-    calculator = new CalculatorController();
+    arithService = new CalculatorService();
+    calculator = new CalculatorController(arithService);
   }
 
   @Test
   public void testAdd() {
-    assertEquals(8.0, calculator.add(5, 3));
+    var a = 5;
+    var b = 3;
+
+    var sum = arithService.arithAdd(a, b);
+    assertEquals(8.0, sum);
   }
 
   @Test
   public void testSubstract() {
-    assertEquals(2.0, calculator.subtract(5, 3));
+    var a = 5;
+    var b = 3;
+
+    var subtract = arithService.arithSubtract(a, b);
+    assertEquals(2.0, subtract);
   }
 
   @Test
   public void testMultiply() {
-    assertEquals(15, calculator.multiply(5, 3));
+    var a = 5;
+    var b = 3;
+
+    var multiply = arithService.arithMultiply(a, b);
+    assertEquals(15, multiply);
   }
 
   @Test
   public void testDivide() {
-    assertEquals(2.5, calculator.divide(5, 2));
+    var a = 15;
+    var b = 3;
+
+    var divide = arithService.arithDivide(a, b);
+    assertEquals(5, divide);
   }
 
   @Test
   void divide_byZero_shouldThrowException() {
-    ArithmeticException ex = assertThrows(
-        ArithmeticException.class,
-        () -> calculator.divide(10, 0));
+    ArithmeticException ex =
+        assertThrows(ArithmeticException.class, () -> calculator.divide(10, 0));
 
     assertEquals("Division with zero is impossible", ex.getMessage());
   }
