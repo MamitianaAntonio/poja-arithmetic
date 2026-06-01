ef6c8ee4b63abbabee46b070b0ec184cc5656ae1
MamitianaAntonio <ratolojanaharyantonio1@gmail.com>
2026-06-01 12:28:14 +0300

    feat : force arith controller to use his service

--- src/main/java/com/antonio/demo/endpoint/rest/controller/health/CalculatorController.java
+++ src/main/java/com/antonio/demo/endpoint/rest/controller/health/CalculatorController.java
@@ -1,31 +1,38 @@
 package com.antonio.demo.endpoint.rest.controller.health;
 
+import com.antonio.demo.endpoint.rest.service.CalculatorService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CalculatorController {
+  private final CalculatorService arithService;
+
+  public CalculatorController(CalculatorService arithService) {
+    this.arithService = arithService;
+  }
+
   @GetMapping("/add")
-  public double add(@RequestParam double a, @RequestParam double b) {
-    return a + b;
+  public long add(@RequestParam long a, @RequestParam long b) {
+    return arithService.arithAdd(a, b);
   }
 
   @GetMapping("/subtract")
-  public double subtract(@RequestParam double a, @RequestParam double b) {
-    return a - b;
+  public double subtract(@RequestParam long a, @RequestParam long b) {
+    return arithService.arithSubtract(a, b);
   }
 
   @GetMapping("/multiply")
-  public double multiply(@RequestParam double a, @RequestParam double b) {
-    return a * b;
+  public double multiply(@RequestParam long a, @RequestParam long b) {
+    return arithService.arithMultiply(a, b);
   }
 
   @GetMapping("/divide")
-  public double divide(@RequestParam double a, @RequestParam double b) {
+  public double divide(@RequestParam long a, @RequestParam long b) {
     if (b == 0) {
       throw new ArithmeticException("Division with zero is impossible");
     }
-    return a / b;
+    return arithService.arithDivide(a, b);
   }
 }
